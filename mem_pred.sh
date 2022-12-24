ee98335869576f5e1bb2c038816004e5b84a595e
Saifali Sayyed <sayyedsaif869@gmail.com>
2022-12-24 18:00:42 +0000

    third commit

--- mem_pred.sh
+++ mem_pred.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-mem=$(tail -1 /home/ubuntu/asadthesisproject/jupyter/output/jupyter/output/mem_pred_output | sed 's/[][]//g' | awk '{printf "%.0f\n", $1}')
-echo $mem >> /home/ubuntu/asadthesisproject/jupyter/output/jupyter/output/mem_pred
-mempredgrthan65=$(cat /home/ubuntu/asadthesisproject/jupyter/output/jupyter/output/mem_pred)
+mem=$(tail -1 /home/ubuntu/asadthesisproject/jupyter/output/mem_pred_output | sed 's/[][]//g' | awk '{printf "%.0f\n", $1}')
+echo $mem >> /home/ubuntu/asadthesisproject/jupyter/output/mem_pred
+mempredgrthan65=$(cat /home/ubuntu/asadthesisproject/jupyter/output/mem_pred)
       if [[ -n "$mempredgrthan65" ]]; then
             python3 mempredscaling.py greater nodockername
-            rm /home/ubuntu/asadthesisproject/jupyter/output/jupyter/output/mem_pred
+            rm /home/ubuntu/asadthesisproject/jupyter/output/mem_pred
       else
             docker_name=$(docker ps --format "{{.Names}}" | grep -i asad-figlet-mempred)
             if [[ -n "$docker_name" ]]; then
